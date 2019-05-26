302128446af939ff448ac1fd125986e39a5a6c3c
Ben Falk <falk.ben@gmail.com>
2019-05-26 15:52:11 -0400

    cutom login/logout templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
--- mysite/zoo_checks/views.py
+++ mysite/zoo_checks/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import login, logout
 from django.shortcuts import render
 
-# Create your views here.
+
+# def login(request):
+#     pass
+
+
+# def logout_view(request):
+#     logout(request)
