e2cc6387dcf95690761460d4b2ef0b2c2477d026
Ben Falk <falk.ben@gmail.com>
2019-05-26 16:56:05 -0400

    Models (#2)
    * init models
    
    * install app in settings
    
    enable models in admin
    
    add migration
    
    * Allauth (#1)
    
    * cutom login/logout templates
    
    * remove custom login/logout templates & app
    
    * allauth for user auth templates/views/urls
    
    * update readme name

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    # path("accounts/", include("django.contrib.auth.urls")),
+    path(
+        "account_management",
+        TemplateView.as_view(template_name="account_management.html"),
+        name="account_management",
+    ),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
