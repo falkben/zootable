8c031e1b8a63b5407ee43fa392e91ac33958a9b6
Ben Falk <falk.ben@gmail.com>
2019-06-01 15:14:24 -0400

    cutomizing the form
    form_list to form_dict

--- zootally/zoo_checks/views.py
+++ zootally/zoo_checks/views.py
@@ -14,23 +14,28 @@ def create_combined_form(exhibits):
     # TODO: counts should default to maximum (across users) for the day
     # TODO: condition should default to median condition (across users) for the day
 
-    form_list = []
+    form_dict = {}
 
     count_kwargs = {"datetime": datetime.now()}
 
     for exhibit in exhibits:
+        form_dict[exhibit] = {}
         for spec in exhibit.species.all():
             spec_count = SpeciesExhibitCount(
                 species=spec, exhibit=exhibit, **count_kwargs
             )
-            form_list.append(SpeciesExhibitCountForm(instance=spec_count))
+            form_dict[exhibit][spec] = {}
+
+            form_dict[exhibit][spec][spec] = SpeciesExhibitCountForm(
+                instance=spec_count
+            )
 
             anim_spec_exhib = Animal.objects.filter(exhibit=exhibit, species=spec)
             for anim in anim_spec_exhib:
                 anim_count = AnimalCount(animal=anim, **count_kwargs)
-                form_list.append(AnimalCountForm(instance=anim_count))
+                form_dict[exhibit][spec][anim] = AnimalCountForm(instance=anim_count)
 
-    return form_list
+    return form_dict
 
 
 def count(request):
@@ -39,7 +44,7 @@ def count(request):
     # if this is a POST request we need to process the form data
     if request.method == "POST":
         # create a form instance and populate it with data from the request:
-        for form in form_list:
+        for form in form_dict:
             # need to figure out whether it's an AnimalCountForm or a SpeciesCountForm
             form = AnimalCountForm(request.POST)
             # check whether it's valid:
@@ -56,10 +61,10 @@ def count(request):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form_list = create_combined_form(exhibits)
+        form_dict = create_combined_form(exhibits)
 
     return render(
-        request, "tally.html", context={"form_list": form_list, "exhibits": exhibits}
+        request, "tally.html", context={"form_dict": form_dict, "exhibits": exhibits}
     )
 
 
