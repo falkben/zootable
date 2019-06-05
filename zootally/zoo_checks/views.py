6cdf7e9db47de6f1f83239dcd0b5a30e5a843b70
Ben Falk <falk.ben@gmail.com>
2019-06-05 00:01:13 -0400

    currently broken
    move to trying to do it all in the view

--- zootally/zoo_checks/views.py
+++ zootally/zoo_checks/views.py
@@ -19,6 +19,61 @@ def home(request):
     return render(request, "home.html", {"exhibits": exhibits})
 
 
+def create_formsets(exhibit, exhibit_species, exhibit_animals, request=None):
+    """
+    Creates formsets in a dictionary in a particular order that we can 
+    render in the template
+    """
+
+    SpeciesCountFormset = inlineformset_factory(
+        Exhibit,
+        SpeciesExhibitCount,
+        form=SpeciesExhibitCountForm,
+        can_delete=False,
+        can_order=False,
+        extra=1,  # one species at a time in "our" order
+        # extra=len(exhibit_species),  # exhibit.species.count(),
+    )
+    AnimalCountFormset = inlineformset_factory(
+        Exhibit,
+        AnimalCount,
+        form=AnimalCountForm,
+        can_delete=False,
+        can_order=False,
+        extra=1,  # one animal at a time, in "our" order
+        # extra=len(exhibit_animals),  # exhibit.animals.count(),
+    )
+
+    # to set the order
+    formset_dict = {}
+    for spec in exhibit_species:
+        # create species formset
+        species_formset = SpeciesCountFormset(
+            instance=exhibit, initial={"species": spec}
+        )
+        species_formset = SpeciesCountFormset(
+            instance=exhibit, initial={"species": spec}
+        )
+
+        spec_anim_list = []
+        spec_anim_formset = []
+        for animal in exhibit_animals.filter(species=spec):
+            spec_anim_list.append(animal)
+            animal_formset = AnimalCountFormset(
+                instance=exhibit, initial={"animal": animal}
+            )
+            spec_anim_formset.append(animal_formset)
+
+        # store in dictionary, using id because that's known unique
+        formset_dict[spec.id] = {}
+        formset_dict[spec.id]["species"] = spec
+        formset_dict[spec.id]["specformset"] = species_formset
+        formset_dict[spec.id]["animals"] = spec_anim_list
+        formset_dict[spec.id]["animalformset"] = spec_anim_formset
+
+    return formset_dict
+
+
 @login_required
 def count(request, exhibit_id):
     # TODO: counts should default to maximum (across users) for the day
@@ -27,34 +82,6 @@ def count(request, exhibit_id):
     exhibit = get_object_or_404(Exhibit, pk=exhibit_id)
     exhibit_species = exhibit.species.all()
     exhibit_animals = exhibit.animals.all()
-
-    SpeciesCountFormset = inlineformset_factory(
-        Exhibit,
-        SpeciesExhibitCount,
-        form=SpeciesExhibitCountForm,
-        can_delete=False,
-        can_order=False,
-        extra=len(exhibit_species),  # exhibit.species.count(),
-    )
-    AnimalCountFormset = inlineformset_factory(
-        Exhibit,
-        AnimalCount,
-        form=AnimalCountForm,
-        can_delete=False,
-        can_order=False,
-        extra=len(exhibit_animals),  # exhibit.animals.count(),
-    )
-
-    # to set the order
-    species_animals_dict = {}
-    for spec in exhibit_species:
-        spec_anim_list = []
-        for animal in exhibit_animals.filter(species=spec):
-            spec_anim_list.append(animal.name)
-        species_animals_dict[spec.common_name] = spec_anim_list
-
-    init_sp_vals = [{"species": sp} for sp in exhibit_species]
-    init_anim_vals = [{"animal": anim} for anim in exhibit_animals]
 
     # if this is a POST request we need to process the form data
     if request.method == "POST":
@@ -74,18 +101,15 @@ def count(request, exhibit_id):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        species_formset = SpeciesCountFormset(instance=exhibit, initial=init_sp_vals)
-        animal_formset = AnimalCountFormset(instance=exhibit, initial=init_anim_vals)
+        formset_dict = create_formsets(exhibit, exhibit_species, exhibit_animals)
 
     return render(
         request,
         "tally.html",
         {
             "exhibit": exhibit,
-            "species_formset": species_formset,
-            "animal_formset": animal_formset,
             "exhibit_animals": list(exhibit_animals),
             "exhibit_species": list(exhibit_species),
-            "species_animals_dict": species_animals_dict,
+            "formset_dict": formset_dict,
         },
     )
