9154a0a2253afe24d190a3da854e398aad073bc1
Ben Falk <falk.ben@gmail.com>
2019-06-08 16:58:39 -0400

    finally fixes the intial data on animal formsets

--- zootally/zoo_checks/views.py
+++ zootally/zoo_checks/views.py
@@ -44,7 +44,8 @@ def get_formset_order(
     # to set the order
     formset_dict = {}
     anim_total = 0
-    for ind, spec in enumerate(exhibit_species.order_by("common_name")):
+    for ind, spec in enumerate(exhibit_species):
+        # should be ordered by name here already, but just in case...
         spec_anim_list = exhibit_animals.filter(species=spec).order_by("name")
         spec_anim_index = list(range(anim_total, spec_anim_list.count() + anim_total))
         anim_total += spec_anim_list.count()
@@ -55,41 +56,60 @@ def get_formset_order(
         formset_dict[spec.id] = {}
         formset_dict[spec.id]["species"] = spec
         formset_dict[spec.id]["formset"] = species_formset[ind]
+
+        # this updates the animal formset with initial values
+        [
+            animals_formset[i].initial.update(animals_formset.initial_extra[i])
+            for i in spec_anim_index
+        ]
         formset_dict[spec.id]["animalformset_index"] = zip(
             spec_anim_list, [animals_formset[i] for i in spec_anim_index]
         )
 
-    return formset_dict
+    return formset_dict, species_formset, animals_formset
 
 
 def get_init_spec_count_form(exhibit, exhibit_species, species_counts_today):
     # TODO: counts should default to maximum (across users) for the day
-    # TODO: eliminate the for loop and do this using database
+    # TODO: eliminate for loop
 
-    # this loop is to create the list of counts for the species
+    # this loop is to create the list of counts for the species *in order of exhibit_species*
     init_sp_counts = [0] * exhibit_species.count()
-    for i, sp in enumerate(exhibit_species.order_by("common_name")):
+    # * note: this unpacks the queryset into a list and should be avoideds
+    for i, sp in enumerate(exhibit_species):
         sp_count = species_counts_today.filter(species=sp)
         if sp_count.count() > 0:
+            # should only be one... but in case there is more than one this takes the max
             init_sp_counts[i] = sp_count.aggregate(Max("count"))["count__max"]
 
-    init_spec = [{"species": obj} for obj in exhibit_species.order_by("common_name")]
-    [init.update({"count": c}) for init, c in zip(init_spec, init_sp_counts)]
-
+    # sets the initial dict
+    init_spec = [
+        {"species": obj, "count": c} for obj, c in zip(exhibit_species, init_sp_counts)
+    ]
     return init_spec
 
 
 def get_init_anim_count_form(exhibit, exhibit_animals, animal_conditions_today):
     # TODO: condition should default to median? condition (across users) for the day
-    init_anim = [
-        {"animal": obj} for obj in exhibit_animals.order_by("species__common_name")
-    ]
+    init_anim = [{"animal": obj} for obj in exhibit_animals]
 
     anims_conds = []
-    for anim in exhibit_animals.order_by("species__common_name", "name"):
-        try:
-            cond = animal_conditions_today.get(animal=anim).condition
-        except ObjectDoesNotExist:
+    for anim in exhibit_animals:
+        # * there can be more than one condition per animal
+        conds = animal_conditions_today.filter(animal=anim)
+        if conds.exists():
+            # getting the conditions
+            cond_vals = conds.values_list("condition", flat=True)
+            # creates a condition "order"
+            cv_order = [
+                AnimalCount.SEEN,
+                AnimalCount.NEEDSATTENTION,
+                AnimalCount.BAR,
+                AnimalCount.MISSING,
+            ]
+            # get's the "worst" condition (highest index in list)
+            cond = cv_order[max([cv_order.index(cv) for cv in cond_vals])]
+        else:
             cond = ""
         anims_conds.append(cond)
 
@@ -113,8 +133,8 @@ def get_exhibit_species(animals, groups):
 def count(request, exhibit_id):
     exhibit = get_object_or_404(Exhibit, pk=exhibit_id)
 
-    exhibit_animals = exhibit.animals.all()
-    exhibit_groups = exhibit.groups.all()
+    exhibit_animals = exhibit.animals.all().order_by("species__common_name", "name")
+    exhibit_groups = exhibit.groups.all().order_by("species__common_name", "name")
 
     exhibit_species = get_exhibit_species(exhibit_animals, exhibit_groups).order_by(
         "common_name"
@@ -149,6 +169,7 @@ def count(request, exhibit_id):
         can_delete=False,
     )
 
+    # TODO: initial values aren't being passed into the formset correctly by default, figure out how to do it without manually editing each form
     init_spec = get_init_spec_count_form(exhibit, exhibit_species, species_counts_today)
     init_anim = get_init_anim_count_form(
         exhibit, exhibit_animals, animal_conditions_today
@@ -165,28 +186,38 @@ def count(request, exhibit_id):
             request.POST, instance=exhibit, initial=init_anim, prefix="animals_formset"
         )
 
+        # needed in the case the form wasn't submitted properly and we have to re-render the form
+        formset_order, species_formset, animals_formset = get_formset_order(
+            exhibit_species, exhibit_animals, species_formset, animals_formset
+        )
+
+        # ! a hack because empty permitted is occassionally set to False!
+        for form in animals_formset:
+            form.empty_permitted = True
+
         # check whether it's valid:
         if species_formset.is_valid() and animals_formset.is_valid():
             # process the data in form.cleaned_data as required
             for form in species_formset:
                 if form.has_changed():
+                    # TODO: move this into mmodel/(form?) and overwrite the save method
                     spec = form.save(commit=False)
                     spec.user = request.user
+
+                    # forcing a new object stored to database on each save because otherwise it always updated
+                    spec.id = None
                     spec.save()
 
             for form in animals_formset:
                 if form.has_changed():
                     anim = form.save(commit=False)
                     anim.user = request.user
+                    # forcing a new object stored to database on each save because otherwise it always updated
+                    anim.id = None
                     anim.save()
 
             # redirect to a new URL:
             return HttpResponseRedirect("/")
-
-        # needed in the case the form wasn't submitted properly and we have to re-render the form
-        formset_order = get_formset_order(
-            exhibit_species, exhibit_animals, species_formset, animals_formset
-        )
 
     # if a GET (or any other method) we'll create a blank form
     else:
@@ -196,7 +227,7 @@ def count(request, exhibit_id):
         animals_formset = AnimalCountFormset(
             instance=exhibit, initial=init_anim, prefix="animals_formset"
         )
-        formset_order = get_formset_order(
+        formset_order, species_formset, animals_formset = get_formset_order(
             exhibit_species, exhibit_animals, species_formset, animals_formset
         )
 
@@ -205,8 +236,6 @@ def count(request, exhibit_id):
         "tally.html",
         {
             "exhibit": exhibit,
-            "exhibit_animals": list(exhibit_animals),
-            "exhibit_species": list(exhibit_species),
             "species_formset": species_formset,
             "animals_formset": animals_formset,
             "formset_order": formset_order,
