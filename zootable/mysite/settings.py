0d9c4b11b742c60be2e6984c29af430888a658a1
Ben Falk <falk.ben@gmail.com>
2019-09-17 15:07:28 -0400

    add http/2 link server push

--- zootable/mysite/settings.py
+++ zootable/mysite/settings.py
@@ -99,6 +99,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_cloudflare_push.middleware.push_middleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
