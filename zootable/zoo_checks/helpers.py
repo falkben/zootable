829cf977ec8faf61f72c237692363cb77154a76f
Ben Falk <falk.ben@gmail.com>
2019-09-25 00:41:46 -0400

    avoid naming weirdness

--- zootable/zoo_checks/helpers.py
+++ zootable/zoo_checks/helpers.py
@@ -6,9 +6,9 @@ def today_time():
     return timezone.localtime().replace(hour=0, minute=0, second=0, microsecond=0)
 
 
-def prior_days(prior_days=3):
+def prior_days(days=3):
     p_days = []
-    for p in range(prior_days):
+    for p in range(days):
         day = today_time() - timezone.timedelta(days=p + 1)
         p_days.append({"year": day.year, "month": day.month, "day": day.day})
 
