d23d0ec91dcf25674a9394be07fab11c7959dab8
Ben Falk <falk.ben@gmail.com>
2019-09-27 13:41:14 -0400

    remove user email from export data (#27)
    * remove user email from export data
    
    sort the dataframe
    
    combine columns across animals/groups/species
    
    split datatime into separate cols
    
    * fix test
    
    * fix other test

--- zootable/tests/test_helpers.py
+++ zootable/tests/test_helpers.py
@@ -2,6 +2,7 @@ import pandas as pd
 from django_mock_queries.query import MockModel, MockSet, MockField
 from django.utils import timezone
 import pytest
+import datetime
 
 from zoo_checks.helpers import qs_to_df, clean_df
 
@@ -13,30 +14,70 @@ class MockFieldIsRelation(MockField):
 
 
 def test_qs_to_df():
-    test_data = [
-        {"mock_name": "john", "email": "john@gmail.com", "dateonlycounted": None},
-        {"mock_name": "jeff", "email": "jeff@hotmail.com", "dateonlycounted": None},
-        {"mock_name": "bill", "email": "bill@gmail.com", "dateonlycounted": None},
-    ]
-    test_df = pd.DataFrame(test_data)
+    # animal count
+    test_data = {
+        "id": 1637,
+        "datecounted": datetime.datetime(2019, 9, 26, 13, 49, 28, 281415),
+        "user_id": 4,
+        "enclosure_id": 12,
+        "condition": "SE",
+        "animal_id": 29,
+        "dateonlycounted": datetime.date(2019, 9, 26),
+    }
+    test_df = pd.DataFrame([test_data])
+    qs = MockSet(MockModel(test_data))
+    df = qs_to_df(qs, [MockFieldIsRelation(k) for k in list(test_data.keys())])
+    assert pd.DataFrame.equals(df, test_df)
 
-    qs = MockSet(
-        MockModel(test_data[0]), MockModel(test_data[1]), MockModel(test_data[2])
-    )
-    df = qs_to_df(qs, [MockFieldIsRelation("mock_name"), MockFieldIsRelation("email")])
+    # group count
+    test_data = {
+        "id": 6,
+        "datecounted": datetime.datetime(2019, 9, 27, 3, 59, 59),
+        "user_id": 1,
+        "enclosure_id": 5,
+        "count_male": 0,
+        "count_female": 0,
+        "count_unknown": 25,
+        "group_id": 1,
+        "dateonlycounted": datetime.date(2019, 9, 26),
+    }
+    test_df = pd.DataFrame([test_data])
+    qs = MockSet(MockModel(test_data))
+    df = qs_to_df(qs, [MockFieldIsRelation(k) for k in list(test_data.keys())])
+    assert pd.DataFrame.equals(df, test_df)
 
+    # species count
+    test_data = {
+        "id": 25,
+        "datecounted": datetime.datetime(2019, 9, 27, 16, 10, 24, 504457),
+        "user_id": 1,
+        "enclosure_id": 5,
+        "count": 3,
+        "species_id": 10,
+        "dateonlycounted": datetime.date(2019, 9, 27),
+    }
+    test_df = pd.DataFrame([test_data])
+    qs = MockSet(MockModel(test_data))
+    df = qs_to_df(qs, [MockFieldIsRelation(k) for k in list(test_data.keys())])
     assert pd.DataFrame.equals(df, test_df)
 
 
 def test_clean_df():
     timestamp = timezone.localtime()
     test_data = [
-        {"mock_name": "john", "email": "john@gmail.com", "datecounted": timestamp}
+        {
+            "id": 1637,
+            "datecounted": timestamp,
+            "user__username": 4,
+            "enclosure__name": "encl_test_name",
+            "condition": "SE",
+            "dateonlycounted": timestamp.date(),
+        }
     ]
     test_df = pd.DataFrame(test_data)
-
     df_clean = clean_df(test_df)
 
-    assert df_clean.loc[0]["datecounted"].to_pydatetime() == timestamp.replace(
+    assert df_clean.loc[0]["date_counted"] == timestamp.replace(tzinfo=None).date()
+    assert df_clean.loc[0]["time_counted"] == timestamp.replace(
         tzinfo=None
-    )
+    ).time().strftime("%H:%M:%S")
