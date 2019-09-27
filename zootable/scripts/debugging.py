20a5371133e5281df6ba46118cb398cc4a772913
Ben Falk <falk.ben@gmail.com>
2019-09-27 13:25:05 -0400

    fix test

--- zootable/scripts/debugging.py
+++ zootable/scripts/debugging.py
@@ -13,19 +13,55 @@ django.setup()
 
 from django.db.models.functions import Cast
 from django.db.models.fields import DateField
-from zoo_checks.models import AnimalCount, Enclosure
+from zoo_checks.models import AnimalCount, Enclosure, GroupCount, SpeciesCount
+from django.db.models.functions import TruncDate
+from django.utils import timezone
+from zoo_checks.helpers import today_time
 
-enclosures = Enclosure.objects.filter(name="Australia")
+enclosures = Enclosure.objects.filter(name__in=["Australia", "Barramundi (ARG 1A)"])
+tzinfo = "America/New York"
+num_days = 1
 
 animal_counts = (
-    AnimalCount.objects.filter(enclosure__in=enclosures)
-    .annotate(dateonlycounted=Cast("datecounted", DateField()))
-    .order_by("dateonlycounted")
-    .distinct("dateonlycounted")
+    AnimalCount.objects.filter(
+        enclosure__in=enclosures,
+        datecounted__lte=timezone.localtime(),
+        datecounted__gt=today_time() - timezone.timedelta(num_days),
+    )
+    .annotate(dateonlycounted=TruncDate("datecounted", tzinfo=tzinfo))
+    .order_by("dateonlycounted", "animal_id")
+    .distinct("dateonlycounted", "animal_id")
+)
+group_counts = (
+    GroupCount.objects.filter(
+        enclosure__in=enclosures,
+        datecounted__lte=timezone.localtime(),
+        datecounted__gt=today_time() - timezone.timedelta(num_days),
+    )
+    .annotate(dateonlycounted=TruncDate("datecounted", tzinfo=tzinfo))
+    .order_by("dateonlycounted", "group_id")
+    .distinct("dateonlycounted", "group_id")
+)
+species_counts = (
+    SpeciesCount.objects.filter(
+        enclosure__in=enclosures,
+        datecounted__lte=timezone.localtime(),
+        datecounted__gt=today_time() - timezone.timedelta(num_days),
+    )
+    .annotate(dateonlycounted=TruncDate("datecounted", tzinfo=tzinfo))
+    .order_by("dateonlycounted", "species_id")
+    .distinct("dateonlycounted", "species_id")
 )
 
+animal_dict = animal_counts.values()[0]
+group_dict = group_counts.values()[0]
+species_dict = species_counts.values()[0]
 
-print(animal_counts)
+print(animal_dict)
+print(animal_dict.keys())
 
-for c in animal_counts:
-    print(c.id, c, "\\", c.datecounted, "\\", c.dateonlycounted)
+print(group_dict)
+print(group_dict.keys())
+
+print(species_dict)
+print(species_dict.keys())
