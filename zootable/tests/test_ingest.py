50b70107f96c76c480a4d40bb59430723088352a
Ben Falk <falk.ben@gmail.com>
2020-05-23 12:23:40 -0400

    tests on ingest using test db

--- zootable/tests/test_ingest.py
+++ zootable/tests/test_ingest.py
@@ -1,12 +1,16 @@
 import pytest
 from xlrd import XLRDError
+import pandas as pd
 
+
+from zoo_checks.models import Enclosure, Species, Animal
 from zoo_checks.ingest import (
     read_xlsx_data,
-    # create_enclosures,
-    # create_species,
+    create_enclosures,
+    create_species,
+    create_animals,
+    validate_input,
     # find_animals_groups,
-    # create_animals,
     # create_groups,
     # get_changesets,
 )
@@ -21,19 +25,51 @@ def test_read_xlsx_data():
     assert df.shape[0] == 5
 
 
+@pytest.mark.django_db
 def test_create_enclosures():
-    pass
+
+    encl_name = "example_enclosure"
+
+    df = pd.DataFrame({"Enclosure": encl_name}, index=[0])
+    create_enclosures(df)
+
+    test_enclosure = Enclosure.objects.get(name=encl_name)
+
+    assert test_enclosure is not None
 
 
+@pytest.mark.django_db
 def test_create_species():
+
+    df = read_xlsx_data("zootable/test_data/example.xlsx")
+    create_species(df)
+
+    for common_name in df["Common"]:
+        sp = Species.objects.get(common_name=common_name)
+        assert sp is not None
+
+
+@pytest.mark.django_db
+def test_create_animals():
+    df = read_xlsx_data("zootable/test_data/example.xlsx")
+
+    # need to first create species, enclosures
+    create_enclosures(df)
+    create_species(df)
+
+    df_validated = validate_input(df)
+    create_animals(df_validated)
+
+    for acc_num in df_validated["Accession"]:
+        anim = Animal.objects.get(accession_number=acc_num)
+        assert anim is not None
+
+
+def test_validate_input():
     pass
 
 
 def test_find_animals_groups():
-    pass
-
-
-def test_create_animals():
     pass
 
 
