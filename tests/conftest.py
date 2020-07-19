3a4ab549a3b4fb875c93839551cf408fb42979a9
Ben Falk <falk.ben@gmail.com>
2020-07-19 17:14:01 -0400

    add fixutres for models
    add a few model tests

---
+++ tests/conftest.py
@@ -0,0 +1,168 @@
+"""
+conftest.py is autodiscovered by pytest 
+https://docs.pytest.org/en/stable/fixture.html#conftest-py-sharing-fixture-functions
+"""
+
+import pytest
+from django.utils.timezone import datetime
+
+from zoo_checks.models import (
+    Animal,
+    AnimalCount,
+    Enclosure,
+    Group,
+    GroupCount,
+    Species,
+    SpeciesCount,
+    User,
+)
+
+
+@pytest.fixture
+def user_base(db):
+    return User.objects.create_user("base")
+
+
+@pytest.fixture
+def enclosure_base(db):
+    return Enclosure.objects.create(name="base")
+
+
+@pytest.fixture
+def species_base(db):
+    return Species.objects.create(
+        common_name="common_base",
+        class_name="class_base",
+        order_name="order_base",
+        family_name="family_base",
+        genus_name="genus_base",
+        species_name="species_base",
+    )
+
+
+@pytest.fixture
+def species_base_count(
+    db, species_base, user_base, enclosure_base, datetimecounted=None
+):
+    if datetimecounted is None:
+        datetimecounted = datetime.now()
+    return SpeciesCount.objects.create(
+        datetimecounted=datetimecounted,
+        datecounted=datetimecounted.date(),
+        species=species_base,
+        user=user_base,
+        enclosure=enclosure_base,
+        count=100,
+    )
+
+
+def animal_factory(
+    name: str,
+    identifier: str,
+    sex: str,
+    accession_number: str,
+    enclosure: Enclosure,
+    species: Species,
+    active: bool = True,
+) -> Animal:
+    return Animal.objects.create(
+        name=name,
+        identifier=identifier,
+        sex=sex,
+        accession_number=accession_number,
+        enclosure=enclosure,
+        species=species,
+        active=active,
+    )
+
+
+@pytest.fixture
+def animal_A(db, species_base, enclosure_base):
+    return animal_factory("A_name", "A_id", "M", "123456", enclosure_base, species_base)
+
+
+def animal_count_factory(
+    datetimecounted: datetime,
+    condition: str,
+    animal: Animal,
+    user: User,
+    enclosure: Enclosure,
+    comment: str = "",
+) -> AnimalCount:
+    return AnimalCount.objects.create(
+        datetimecounted=datetimecounted,
+        datecounted=datetimecounted.date(),
+        condition=condition,
+        comment=comment,
+        animal=animal,
+        user=user,
+        enclosure=enclosure,
+    )
+
+
+@pytest.fixture
+def animal_count_A_BAR(db, animal_A, user_base, enclosure_base, datetimecounted=None):
+    if datetimecounted is None:
+        datetimecounted = datetime.now()
+    return animal_count_factory(
+        datetimecounted, "BA", animal_A, user_base, enclosure_base
+    )
+
+
+@pytest.fixture
+def group_B(db, enclosure_base, species_base):
+    return Group.objects.create(
+        active=True,
+        accession_number="654321",
+        population_male=1,
+        population_female=2,
+        population_unknown=3,
+        population_total=6,
+        species=species_base,
+        enclosure=enclosure_base,
+    )
+
+
+def group_count_factory(
+    datetimecounted: datetime,
+    group: Group,
+    user: User,
+    enclosure: Enclosure,
+    count_total: int,
+    count_seen: int,
+    count_not_seen: int,
+    count_bar: int,
+    needs_attn: bool,
+    comment: str = "",
+) -> GroupCount:
+    return GroupCount.objects.create(
+        group=group,
+        datetimecounted=datetimecounted,
+        datecounted=datetimecounted.date(),
+        user=user,
+        enclosure=enclosure,
+        count_total=count_total,
+        count_seen=count_seen,
+        count_not_seen=count_not_seen,
+        count_bar=count_bar,
+        needs_attn=needs_attn,
+        comment=comment,
+    )
+
+
+@pytest.fixture
+def group_B_count(db, group_B, user_base, enclosure_base, datetimecounted=None):
+    if datetimecounted is None:
+        datetimecounted = datetime.now()
+    return group_count_factory(
+        datetimecounted,
+        group_B,
+        user_base,
+        enclosure_base,
+        count_total=6,
+        count_seen=3,
+        count_not_seen=0,
+        count_bar=1,
+        needs_attn=2,
+        comment="",
+    )
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,93 +1,43 @@
 """ test models """
 
-from datetime import date, datetime
-
-import pytest
 
 from zoo_checks.models import (
     Animal,
-    AnimalCount,
     Enclosure,
     Group,
-    GroupCount,
     Species,
-    SpeciesCount,
-    User,
 )
 
 
-@pytest.fixture
-def create_animal_factory(
-    db,
-    name: str,
-    identifier: str,
-    sex: str,
-    accession_number: str,
-    enclosure: Enclosure,
-    species: Species,
-    active: bool = True,
-) -> Animal:
-    return Animal.objects.create(
-        name=name,
-        identifier=identifier,
-        sex=sex,
-        enclosure=enclosure,
-        active=active,
-        accession_number=accession_number,
-        species=species,
-    )
+def test_animal_instance(animal_A: Animal) -> None:
+    assert isinstance(animal_A, Animal)
+    assert animal_A.name == "A_name"
 
 
-@pytest.fixture
-def animal_A(db, create_animal_factory, enclosure, species, active=True) -> Animal:
-    return create_animal_factory(
-        "A", "A animal", "M", "543210", enclosure, species, active
-    )
+def test_species_instance(species_base: Species):
+    assert isinstance(species_base, Species)
+    assert species_base.genus_name == "genus_base"
 
 
-@pytest.fixture
-def animal_B(db, create_animal_factory, enclosure, species, active=True) -> Animal:
-    return create_animal_factory(
-        "B", "B animal", "F", "012345", enclosure, species, active
-    )
+def test_enclosure_instance(enclosure_base: Enclosure):
+    assert isinstance(enclosure_base, Enclosure)
+    assert enclosure_base.name == "base"
 
 
-@pytest.fixture
-def animal_count_factory(db, animal: Animal, user: User, enclosure: Enclosure):
-    def create_animal_count(
-        condition: str, datetimecounted: datetime, datecounted: date, comment: str = "",
-    ):
-        animal_count = AnimalCount.objects.create(
-            condition=condition,
-            comment=comment,
-            datetimecounted=datetimecounted,
-            datecounted=datecounted,
-            user=user,
-            animal=animal,
-            enclosure=enclosure,
-        )
-        return animal_count
-
-    return create_animal_count
+def test_group_instance(group_B: Group):
+    assert isinstance(group_B, Group)
+    assert group_B.accession_number == "654321"
 
 
-def test_should_create_animal(animal_A: Animal) -> None:
-    enclosure = create_enclosure("name")
-    species = create_species("lkjlkj")
-    assert isinstance(animal_A, Animal, enclosure, species)
+def test_accession_numbers_total(
+    enclosure_base: Enclosure, animal_A: Animal, group_B: Group
+):
+    num = enclosure_base.accession_numbers_total()
+    assert num == 2
 
 
-# pytest fixtures for creating models
-
-
-# create some test data
-# users
-# species
-# animals
-# groups
-# animal counts
-# group counts
-# species counts
-
-
-# test Enclosure counts_on_day
+# TODO:
+# def test_accession_numbers_observed(
+#     enclosure_base: Enclosure, animal_A: Animal, group_B: Group
+# ):
+#     ...
