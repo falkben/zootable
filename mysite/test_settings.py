7bcff68f268b6604820b06134f47e96ffffdfb7e
Ben Falk <falk.ben@gmail.com>
2023-10-01 15:50:43 -0400

    staticfiles config override in test settings update for 4.2

--- mysite/test_settings.py
+++ mysite/test_settings.py
@@ -3,7 +3,11 @@ from .settings import *
 # Override STATICFILES_STORAGE setting
 # Avoid "ValueError: Missing staticfiles manifest entry" during testing
 # see: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#django.contrib.staticfiles.storage.ManifestStaticFilesStorage.manifest_strict
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 # faster password hashing for test
 # can also get around this by doing force_login()
