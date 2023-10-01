880bb6860a0edb6afc67176de3980b83982e1d1f
Ben Falk <falk.ben@gmail.com>
2023-10-01 15:17:17 -0400

    update deps (#260)
    * update deps
    
    * add middleware for allauth

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 # add django-debug-toolbar if installed
